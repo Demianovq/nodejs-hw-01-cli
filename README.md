9d196c8a123c1736e012c154e2cf22aed09ae762
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-05-15 20:51:10 +0300

    adds

---
+++ README.md
@@ -0,0 +1,7 @@
+CLI додаток
+
+https://monosnap.com/file/ZHWwGofykw8w2s0Knc0nStbuU2qyPq
+
+https://monosnap.com/file/Jqcl3UTUatRoiWYe2k8pw43Q8xiyzJ
+
+https://monosnap.com/file/eVG1AZIET1Pq7UDdEVebVt8JW5UIso
---
+++ index.js
@@ -0,0 +1,40 @@
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+const options = program.opts();
+invokeAction(options);
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      console.table(contactsList);
+      break;
+
+    case "get":
+      const searchedContact = await contacts.getContactById(id);
+      console.log(searchedContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
